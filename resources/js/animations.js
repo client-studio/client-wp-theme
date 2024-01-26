4f9ebfc6e4fd07a5a9fdb259a89cb852bdb554f2
client.studio <hello@client.fi>
2024-01-26 09:22:17 +0200

    U

--- resources/js/animations.js
+++
@@ -1,9 +0,0 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-
-export default class Animations {
-   logo(div) {
-      console.log("animations ok")
-   }
-}
